0466e7ea2b0b3b4ae824ab8f3a5bdcbc48ee9d49
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2021-12-14 21:47:35 +0000

    Adding CSS styles.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,17 +4,52 @@
     const title = "Todos App"
 </script>
 
+<style>
+    .todos-container {
+        width: 100%;
+        max-width: 42rem;
+        margin: 4rem auto 0 auto;
+    }
+
+    .new {
+        margin-bottom: 0.5rem;
+    }
+
+    .new input {
+        font-size: 28px;
+        width: 100%;
+        padding: 0.5em 1em 0.3em 1em;
+        text-align: center;
+        background: rgba(255, 255, 255, 0.5);
+        border-radius: 8px;
+        outline: none;
+    }
+
+    .todos-container :global(input) {
+        border: none;
+    }
+
+    .todos-container :global(input:focus-visible) {
+        border: 2px solid var(--primary-color) !important;
+        outline: none;
+        box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.1);
+    }
+</style>
+
 <svelte:head>
     <title>{title}</title>
 </svelte:head>
 
-<h1>{title}</h1>
+<div class="todos-container">
+    <h1>{title}</h1>
+    
+    <form action="" method="" class="new">
+        <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo">
+    </form>
+    
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+</div>
 
-<form action="" method="">
-    <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo">
-</form>
-
-<TodoItem />
-<TodoItem />
-<TodoItem />
 
