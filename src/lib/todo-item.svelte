9d03cf032d0c58d6aec48fe1849b88c06fb1eb73
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2021-12-28 02:15:07 +0000

    Add PATCH endpoint - mark as done btn.

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -73,7 +73,7 @@
         opacity: 1;
     }
 
-    /* .done {
+    .done {
         transform: none;
         opacity: 0.4;
         filter: drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.1));
@@ -81,15 +81,15 @@
 
     .done .toggle-btn {
         background-image: url("data:image/svg+xml,%3Csvg width='22' height='16' viewBox='0 0 22 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 1.5L7.4375 14.5L1.5 8.5909' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
-    } */
+    } 
 </style>
 
 
 <!--HTML SECTION-->
-<div class="todo-item">
-    <form action="" method="">
-        <input type="hidden" name="done" value="">
-        <button aria-label="Mark done/not done" class="toggle-btn"></button>
+<div class="todo-item" class:done={todo.done}>
+    <form action="/todos/{todo.uid}.json?_method=patch" method="post">
+        <input type="hidden" name="done" value="{todo.done ? '' : 'true'}">
+        <button aria-label="Mark to do as {todo.done ? 'not done' : 'done'}" class="toggle-btn"></button>
     </form>
 
     <form action="/todos/{todo.uid}.json?_method=patch" method="post" class="edit-todo">
