51e67fa1b094bb17f76124fa5f2b03ee04b78cc9
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2021-12-30 19:47:13 +0000

    Enhance HTML forms.

---
+++ src/lib/actions/form.ts
@@ -0,0 +1,36 @@
+export const enhance = (form: HTMLFormElement, {result}) => {
+
+  const handleSubmit = async (event: Event) => {
+    event.preventDefault()
+
+    try {
+      const body = new FormData(form)
+      const res = await fetch(form.action, {
+        method: form.method,
+        headers: {
+          accept: "application/json"
+        },
+        body
+      })
+
+      if (res.ok) {
+        result(res, form)
+      } else {
+        console.log("ERROR ", await res.text());
+        
+      }
+
+    } catch (error) {
+      console.log('Could not submit: ', error);    
+    }
+  }
+  
+
+  form.addEventListener("submit", handleSubmit)
+
+  return {
+    destroy() {
+      form.removeEventListener("submit", handleSubmit)
+    }
+  }
+}
